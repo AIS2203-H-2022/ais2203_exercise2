c9bd67c192db882b858c1cc9606dec9c9584aebc
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2022-09-02 08:48:25 +0200

    solution

--- src/json_parser.cpp
+++ src/json_parser.cpp
@@ -1,12 +1,30 @@
 
 #include "exercise/json_parser.hpp"
 
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <sstream>
+
 sensor_data_t parse_json(const std::string &jsonStr) {
-    //TODO return a populated sensor_data_t object based on the content of jsonStr
-    return {}; // <-- replace me (returns empty sensor_data_t object)
+
+    sensor_data_t data{};
+
+    nlohmann::json json = nlohmann::json::parse(jsonStr);
+    data.imu.is_valid = json["imu"]["is_valid"].get<bool>();
+    data.imu.yaw = json["imu"]["yaw"].get<double>();
+    data.imu.pitch = json["imu"]["pitch"].get<double>();
+    data.imu.roll = json["imu"]["roll"].get<double>();
+    data.accelerometer.is_valid = json["accelerometer"]["is_valid"].get<bool>();
+    data.accelerometer.x = json["accelerometer"]["x"].get<double>();
+    data.accelerometer.y = json["accelerometer"]["y"].get<double>();
+    data.accelerometer.z = json["accelerometer"]["z"].get<double>();
+
+    return data;
 }
 
 sensor_data_t parse_json(const std::filesystem::path &jsonFile) {
-    //TODO return a populated sensor_data_t object based on the content of jsonFile
-    return {}; // <-- replace me (returns empty sensor_data_t object)
+    std::ifstream f(jsonFile);
+    std::stringstream strStream;
+    strStream << f.rdbuf(); //read the fil
+    return parse_json(strStream.str());
 }
