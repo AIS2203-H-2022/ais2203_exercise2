2b9ad2bf6107574902c5395e671dcff7ed51c715
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2022-09-02 08:50:22 +0200

    update

--- src/json_parser.cpp
+++ src/json_parser.cpp
@@ -1,6 +1,8 @@
 
 #include "exercise/json_parser.hpp"
 
+#include <nlohmann/json.hpp>
+
 sensor_data_t parse_json(const std::string &jsonStr) {
     //TODO return a populated sensor_data_t object based on the content of jsonStr
     return {}; // <-- replace me (returns empty sensor_data_t object)
